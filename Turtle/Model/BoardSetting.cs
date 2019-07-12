0c92e50459afb39dc9e06b5a57911068a096fd9b
Eamonn Byrne <eamonnjames@outlook.com>
2019-07-12 21:22:12 +0100

    adding files

---
+++ Turtle/Model/BoardSetting.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Turtle.Model
+{
+    class BoardSetting
+    {
+        public int width { get; set; }
+        public int height { get; set; }
+    }
+}
---
+++ Turtle/Model/Direction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Turtle.Model
+{   
+    public class Direction
+    {
+        public string _direction { get; set; }
+
+        public Direction(string direction)
+        {
+            _direction = direction;
+        }
+        
+    }
+}
