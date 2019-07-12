1b7ee9027a215e3b5e4331e087c707889a24a8f5
Eamonn Byrne <eamonnjames@outlook.com>
2019-07-12 21:21:16 +0100

    adding files

---
+++ Turtle/Grid/Coordinate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Turtle.Grid
+{
+    class Coordinate
+    {
+        public Coordinate(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+        }
+        public int x { get; set; }
+        public int y { get; set; }
+        public int storedValue { get; set; }
+    }
+}
---
+++ Turtle/Grid/Grid.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Turtle.Grid
+{
+    class Grid
+    {
+        public Grid(int width, int length)
+        {
+            coordinates = new List<Coordinate>();
+            for (int i = 0; i < width - 1; i++)
+            {
+                for (int k = 0; k < length + 1; k++)
+                {
+                    coordinates.Add(new Coordinate(k, i));
+                }
+            }
+        }
+        List<Coordinate> coordinates;
+        int width { get; set; }
+        int length { get; set; }
+        public int accessCoordinate(int x, int y)
+        {
+            return coordinates.Where(coord => coord.x == x && coord.y == y)
+                              .FirstOrDefault().storedValue;
+        }
+        public void assignValue(int x, int y, int value)
+        {
+            coordinates.Where(coord => coord.x == x && coord.y == y)
+                       .FirstOrDefault().storedValue = value;
+        }
+    }
+}
