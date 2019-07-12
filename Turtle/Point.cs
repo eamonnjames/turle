9bacd515f167be7b3b465a69c30b8cfd353dc02b
Eamonn Byrne <eamonnjames@outlook.com>
2019-07-12 21:19:42 +0100

    adding files

---
+++ Turtle/Point.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Turtle
+{
+   public class Point : IComparable
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            Point p1 = (Point)obj;
+
+            if(p1.x == x && p1.y == y)
+            {
+                return 0;
+            }
+
+            return 1;
+        }
+    }
+}
---
+++ Turtle/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Linq;
+using Turtle.Settings;
+
+namespace Turtle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var getMoves = GameSettings.getMoves("Moves");            
+            var p1 = new State.Player();
+            var TurtlePoint = new Point();
+            var Direction = "North";
+
+            for (int i = 0;i < getMoves.Count; i++)
+            {
+                if (i == 0)
+                {
+                    var startingpos = GameSettings.GameSettingGetStartingPosition("Starting Position", "Settings");
+                    TurtlePoint.x = startingpos.x;
+                    TurtlePoint.y = startingpos.y;                    
+                }
+                
+                if(getMoves[i] == "move")
+                {
+                    if(Direction == "North")
+                    {
+                        TurtlePoint.y = TurtlePoint.y - 1;
+                        p1.Move(TurtlePoint);
+                    }
+                    if(Direction == "East")
+                    {
+                        TurtlePoint.x = TurtlePoint.x + 1;
+                        p1.Move(TurtlePoint);
+                    }
+
+                    if(Direction == "South")
+                    {
+                        TurtlePoint.y = TurtlePoint.y + 1;
+                        p1.Move(TurtlePoint);
+                    }
+
+                    if(Direction == "West")
+                    {
+                        TurtlePoint.x = TurtlePoint.x - 1;
+                        p1.Move(TurtlePoint);
+                    }
+                    
+                }
+
+                if(p1.State.GetType().Name == "MineHit")
+                {
+                    Console.WriteLine("Player has hit mine Game Over");
+                    break;
+                }
+
+                if(p1.State.GetType().Name == "InVaildMove")
+                {
+                    Console.WriteLine("Invaild Move, check moves.txt");
+                    break;
+                }
+                if(p1.State.GetType().Name == "FoundExit")
+                {
+                    Console.WriteLine("Turtle has found the exit!!!!");
+                }
+
+                if (getMoves[i] == "rotate")
+                {
+                    Direction = GetUpdatedDirection(Direction);
+                }
+            }
+            Console.ReadLine();
+        }
+        private static string GetUpdatedDirection(string direction)
+        {
+            switch(direction)
+            {
+                case "North": return "East";
+                case "East": return "South";
+                case "South": return "West";
+                case "West": return "North";
+            }
+            return "no value found";
+        }
+    }
+}
